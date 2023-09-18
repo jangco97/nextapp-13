de1323a990bc2217f2426205b3274e001c24f547
jangco97 <qkrnslehd@naver.com>
2023-09-18 12:58:33 +0900

    layout, global.css, package.json 커밋하기

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['res.cloudinary.com', 'via.placeholder.com']//도메인 허용하기
+    }
+}
 
 module.exports = nextConfig
