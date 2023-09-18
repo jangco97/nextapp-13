f47c95a54218e7567dc714205592261e8684eb2b
jangco97 <qkrnslehd@naver.com>
2023-09-18 19:22:57 +0900

    chat 컴포넌트 conversation 타입 지정

--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -1,8 +1,8 @@
-import { TUserWithChat } from '@/types'
-import React, { useEffect, useRef } from 'react'
-import Input from './Input';
-import ChatHeader from './ChatHeader';
-import Message from './Message';
+import { TConversation, TUserWithChat } from "@/types";
+import React, { useEffect, useRef } from "react";
+import Input from "./Input";
+import ChatHeader from "./ChatHeader";
+import Message from "./Message";
 interface ChatProps {
   currentUser: TUserWithChat;
   receiver: {
@@ -10,52 +10,67 @@ interface ChatProps {
     receiverName: string;
     receiverImage: string;
   };
-  setLayout: (layout: boolean) => void
+  setLayout: (layout: boolean) => void;
 }
-const Chat = ({currentUser, receiver, setLayout}:ChatProps) => {
+
+const Chat = ({ currentUser, receiver, setLayout }: ChatProps) => {
   const messagesEndRef = useRef<null | HTMLDivElement>(null);
-  
+
   const scrollToBottom = () => {
-    messagesEndRef?.current?.scrollIntoView({ behavior: "smooth"})
-  }
+    messagesEndRef?.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   useEffect(() => {
-    scrollToBottom()
-  })
+    scrollToBottom();
+  });
 
-  const conversation = currentUser?.conversations.find((conversation) => conversation.users.find((user) => user.id === receiver.receiverId))
-  if(!receiver.receiverName || !currentUser){
-    return <div className="w-full h-full"></div>
+  const conversation: TConversation | undefined =
+    currentUser?.conversations.find((conversation) =>
+      conversation.users.find((user) => user.id === receiver.receiverId)
+    );
+  if (!receiver.receiverName || !currentUser) {
+    return <div className="w-full h-full"></div>;
   }
   return (
-    <div className='w-full'>
+    <div className="w-full">
       <div>
-        <ChatHeader setLayout={setLayout} receiverName={receiver.receiverName} receiverImage={receiver.receiverImage} lastMessageTime={conversation?.messages.filter((message) => message.receiverId === currentUser.id).slice(-1)[0]?.createdAt}/>
-      </div>
-      <div className='flex flex-col gap-8 p-4 overflow-auto h-[calc(100vh_-_60px_-_70px_-_80px)]'>
-        {conversation && 
-            conversation.messages.map((message) => {
-              return (
-                <Message 
-                  key={message.id}
-                  isSender = {message.senderId === currentUser.id}
-                  messageText = {message.text}
-                  messageImage = {message.image}
-                  messageTime = {message.createdAt}
-                  receiverName = {receiver.receiverName}
-                  receiverImage = {receiver.receiverImage}
-                  senderImage = {currentUser?.image}
-                />
-              ) 
-            })
+        <ChatHeader
+          setLayout={setLayout}
+          receiverName={receiver.receiverName}
+          receiverImage={receiver.receiverImage}
+          lastMessageTime={
+            conversation?.messages
+              .filter((message) => message.receiverId === currentUser.id)
+              .slice(-1)[0]?.createdAt
           }
-        <div ref={messagesEndRef}/>
+        />
+      </div>
+      <div className="flex flex-col gap-8 p-4 overflow-auto h-[calc(100vh_-_60px_-_70px_-_80px)]">
+        {conversation &&
+          conversation.messages.map((message) => {
+            return (
+              <Message
+                key={message.id}
+                isSender={message.senderId === currentUser.id}
+                messageText={message.text}
+                messageImage={message.image}
+                messageTime={message.createdAt}
+                receiverName={receiver.receiverName}
+                receiverImage={receiver.receiverImage}
+                senderImage={currentUser?.image}
+              />
+            );
+          })}
+        <div ref={messagesEndRef} />
       </div>
       <div>
-        <Input receiverId={receiver?.receiverId} currentUserId={currentUser?.id}/>
+        <Input
+          receiverId={receiver?.receiverId}
+          currentUserId={currentUser?.id}
+        />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Chat
+export default Chat;
