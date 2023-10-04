70628672aad10eaf5e2ce98c55262c5f112ef3d0
jangco97 <qkrnslehd@naver.com>
2023-10-04 10:13:54 +0900

    react-gallery를 이용해 여러장의 이미지 처리하기

--- src/components/products/ProductHead.tsx
+++ src/components/products/ProductHead.tsx
@@ -1,34 +1,49 @@
+"use client";
 import { User } from "../../../prisma/generated/client";
 import React from "react";
 import Heading from "../Heading";
-import Image from "next/image";
 import HeartButton from "../HeartButton";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import ProductImage from "./ProductImage";
 
 interface ProductHeadProps {
   title: string;
-  imageSrc: string;
+  imageSrc: string[];
   id: string;
+  productId: string;
+  editFunc: () => void;
+  deleteFunc: () => void;
   currentUser?: User | null;
+  productUserId: string;
 }
 const ProductHead = ({
   title,
   imageSrc,
   id,
+  productId,
+  editFunc,
+  deleteFunc,
   currentUser,
+  productUserId,
 }: ProductHeadProps) => {
   return (
     <>
       <Heading title={title} />
-      <div className="w-full h-[60vh] overflow-hidden rounded-xl relative">
-        <Image
-          src={imageSrc}
-          fill
-          className="object-cover w-full"
-          alt="product"
-        />
-        <div className="absolute top-3 right-3">
-          <HeartButton productId={id} currentUser={currentUser} />
+      {productId === currentUser?.id && (
+        <div>
+          <button onClick={editFunc}>수정하기</button>
+          <button onClick={deleteFunc}>삭제하기</button>
         </div>
+      )}
+      <div className="w-full h-[60vh] rounded-xl relative overflow-hidden">
+        <div>
+          <ProductImage imageSrc={imageSrc} />
+        </div>
+        {currentUser?.id !== productUserId && (
+          <div className="absolute top-3 right-3">
+            <HeartButton productId={id} currentUser={currentUser} />
+          </div>
+        )}
       </div>
     </>
   );
