546391fe1ea0b16eaf9c20bde93f296026a027ec
jangco97 <qkrnslehd@naver.com>
2023-10-04 10:15:57 +0900

    이미지 여러장 업로드 할 수 있도록 개선하기

--- src/app/products/upload/page.tsx
+++ src/app/products/upload/page.tsx
@@ -1,19 +1,27 @@
 "use client";
-import Button from "@/components/Button";
 import Container from "@/components/Container";
 import Heading from "@/components/Heading";
 import ImageUpload from "@/components/ImageUpload";
+import { Modal, Button } from "antd";
+import DaumPostcode from "react-daum-postcode";
 import Input from "@/components/Input";
 import CategoryInput from "@/components/categories/CategoryInput";
-import { categories } from "@/components/categories/Categories";
+import { mainCategories } from "@/components/categories/Categories";
+import { subCategories } from "@/components/categories/Categories";
 import dynamic from "next/dynamic";
-import React, { useState } from "react";
+import React, { MouseEvent, useState } from "react";
 import { FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import { useRouter } from "next/navigation";
 import axios from "axios";
+
+export interface Address {
+  address: string;
+  zonecode: string;
+}
 const ProductUploadPage = () => {
   const [isLoading, setIsLoading] = useState(false);
-  const router = useRouter()
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
   const {
     register,
     handleSubmit,
@@ -25,41 +33,129 @@ const ProductUploadPage = () => {
     defaultValues: {
       title: "",
       description: "",
-      category: "",
+      categories: [],
       latitude: 33.5563,
       longitude: 126.8,
-      imageSrc: "",
+      address: "",
+      addressDetail: "",
+      imageSrc: [],
       price: 1,
     },
   });
+
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
-    axios.post('/api/products', data).then((response) => {
-      router.push(`/products/${response.data.id}`)
-    })
-    .catch((err) => {
-      console.log(err)
-    })
-    .finally(() => {setIsLoading(false)})
+    axios
+      .post("/api/products", data)
+      .then((response) => {
+        router.push(`/products/${response.data.id}`);
+      })
+      .catch((err) => {
+        console.log(err);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
   };
-  const imageSrc = watch('imageSrc')
-  const category = watch('category')
-  const latitude = watch('latitude')
-  const longitude = watch('longitude')
-  const KakaoMap = dynamic(() => import('../../../components/KakaoMap'), {
-    ssr: false
-  })
+  const imageSrc = watch("imageSrc");
+  const categories = watch("categories");
+  const latitude = watch("latitude");
+  const longitude = watch("longitude");
+  const address = watch("address");
+  const KakaoMap = dynamic(() => import("../../../components/KakaoMap"), {
+    ssr: false,
+  });
 
-  const setCustomValue = (id:string, value: any) => {
-    setValue(id, value)
-  }
-  
+  const setCustomValue = (id: string, value: any) => {
+    setValue(id, value);
+  };
+  const setCategory = (value: [string, string]) => {
+    setValue("categories", [...value]);
+  };
+  const setImageSrc = (value: [string]) => {
+    setValue("imageSrc", [...value, ...imageSrc]);
+  };
+  const updateImageSrc = (value: string[]) => {
+    setValue("imageSrc", [...value]);
+  };
+  const FirstCategory = mainCategories.find(
+    (element) => element.categoryId === categories[0]
+  );
+  const SecondCategory = subCategories.find(
+    (element) => element.categoryId === categories[1]
+  );
+
+  const showModal = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsModalOpen(true);
+  };
+
+  const handleOk = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsModalOpen(false);
+  };
+
+  const handleCancel = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    setIsModalOpen(false);
+  };
+  const onCompleteAddressSearch = (data: Address) => {
+    setValue("address", data.address);
+    getLatLng(data.address);
+    setIsModalOpen(false);
+  };
+  const getLatLng = async (address: string) => {
+    const data = await axios.get(
+      `https://dapi.kakao.com/v2/local/search/address.json?query=${address}`,
+      {
+        headers: {
+          Authorization: `KakaoAK 23ee4bf9ccd8241e7a44f29dbd9e80f3`,
+        },
+      }
+    );
+    const latitude = parseFloat(data.data.documents[0].y);
+    const longitude = parseFloat(data.data.documents[0].x);
+    if (!latitude || !longitude) return;
+    setValue("latitude", latitude);
+    setValue("longitude", longitude);
+    console.log(latitude, longitude);
+  };
+  const isOver = imageSrc.length >= 3;
   return (
     <Container>
+      {isModalOpen && (
+        <Modal
+          title="대한민국 주소"
+          open={true}
+          onOk={handleOk}
+          onCancel={handleCancel}
+          okText="확인"
+          cancelText="취소"
+          footer={[
+            <Button key="back" onClick={handleCancel}>
+              취소
+            </Button>,
+            <Button key="submit" onClick={handleOk}>
+              확인
+            </Button>,
+          ]}
+        >
+          <DaumPostcode onComplete={onCompleteAddressSearch} />
+        </Modal>
+      )}
       <div className="max-w-screen-lg mx-auto">
         <form className="flex flex-col gap-8" onSubmit={handleSubmit(onSubmit)}>
-          <Heading title="Product Upload" subtitle="Upload your product" center={false}/>
-          <ImageUpload onChange={(value) => setCustomValue('imageSrc', value)} value={imageSrc}/>
+          <Heading
+            title="Product Upload"
+            subtitle="Upload your product"
+            center={false}
+          />
+          <ImageUpload
+            onChange={(value: string) => setImageSrc([value])}
+            imageSrc={imageSrc}
+            disabled={isOver}
+            updateImageSrc={(value: string[]) => updateImageSrc(value)}
+          />
           <Input
             id="title"
             label="Title"
@@ -88,17 +184,48 @@ const ProductUploadPage = () => {
             required
           />
           <hr />
+          <div>
+            {categories[0] && FirstCategory?.label}{" "}
+            {categories[1] && ">" + SecondCategory?.label}
+          </div>
           <div className="grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
-            {categories.map((item) => (
+            {mainCategories.map((item) => (
               <div key={item.label} className="col-span-1">
-                <CategoryInput onClick={(category) => setCustomValue('category', category)} selected={category === item.path} label={item.label} icon={item.icon} path={item.path}/>
+                <CategoryInput
+                  onClick={(a: string, b: string) => setCategory([a, b])}
+                  label={item.label}
+                  categoryId={item.categoryId}
+                  secondCategory={item.secondCategory}
+                />
               </div>
             ))}
           </div>
           <hr />
-          <KakaoMap setCustomValue={setCustomValue} latitude={latitude} longitude={longitude}/> 
+          <div className="mt-4">
+            <label>주소</label>
+            <div className="flex ">
+              <Button onClick={showModal}>우편번호 검색</Button>
+            </div>
+            <input
+              className="w-full h-34 mt-3 pl-3 border"
+              readOnly
+              value={address}
+            />
+            <input
+              id="addressDetail"
+              className="w-full h-34 mt-3 pl-3 border"
+              placeholder="상세주소를 입력해주세요."
+              {...register("addressDetail", { required: true })}
+            />
+          </div>
+          <hr />
+          <KakaoMap
+            setCustomValue={setCustomValue}
+            latitude={latitude}
+            longitude={longitude}
+          />
           {/* 빌드타임이 아닌 런타임에 불러옴 */}
-          <Button label="상품 업로드" />
+          <Button htmlType="submit">상품 업로드</Button>
         </form>
       </div>
     </Container>
--- src/components/ImageUpload.tsx
+++ src/components/ImageUpload.tsx
@@ -4,36 +4,70 @@ import React from "react";
 import { TbPhotoPlus } from "react-icons/tb";
 interface ImageUploadProps {
   onChange: (value: string) => void;
-  value: string;
+  imageSrc: string[];
+  disabled?: boolean;
+  updateImageSrc: (value: string[]) => void;
 }
 
-const ImageUpload = ({ onChange, value }: ImageUploadProps) => {
+const ImageUpload = ({
+  onChange,
+  imageSrc,
+  disabled,
+  updateImageSrc,
+}: ImageUploadProps) => {
   const handleUpload = (result: any) => {
     onChange(result.info.secure_url);
+    console.log(result.info.secure_url, "imageTEST");
   };
-const uploadPreset = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET
+  console.log(imageSrc, "localImageTEST");
+  const uploadPreset = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_PRESET;
+  const handleDelete = (image: string) => {
+    const currentIndex = imageSrc.indexOf(image);
+    let newImages = [...imageSrc];
+    newImages.splice(currentIndex, 1);
+    updateImageSrc(newImages);
+  };
   return (
-    <CldUploadWidget
-      onUpload={handleUpload}
-      uploadPreset={uploadPreset}
-      options={{ maxFiles: 1 }}
-    >
-      {({ open }) => {
-        return (
-          <div
-            onClick={() => open?.()}
-            className="relative flex flex-col items-center justify-center gap-4 p-20 transition border-2 border-dashed cursor-pointer hover:opacity-70 border-neutral-300 text-neutral-300"
-          >
-            <TbPhotoPlus size={50} />
-            {value && (
-              <div className="absolute inset-0 w-full h-full">
-                <Image fill style={{ objectFit: "cover" }} src={value} alt="" />
+    <>
+      <div className="flex justify-center">
+        <CldUploadWidget
+          onUpload={handleUpload}
+          uploadPreset={uploadPreset}
+          options={{ maxFiles: 3 }}
+        >
+          {({ open }) => {
+            return (
+              <div
+                onClick={() => open?.()}
+                className="relative flex flex-col items-center justify-center overflow-hidden gap-4 h-[40vh] w-[30vw] p-20 transition border-2 border-dashed cursor-pointer hover:opacity-70 border-neutral-300 text-neutral-300"
+              >
+                <TbPhotoPlus size={50} />
+
+                <>
+                  {imageSrc[0] && (
+                    <div className="w-full h-full">
+                      <Image
+                        fill
+                        style={{ objectFit: "cover" }}
+                        src={imageSrc[0]}
+                        alt="1"
+                      />
+                    </div>
+                  )}
+                </>
               </div>
-            )}
+            );
+          }}
+        </CldUploadWidget>
+      </div>
+      <div className="flex-grow h-[300px] border flex  items-center justify-center">
+        {imageSrc.map((image, index) => (
+          <div key={index} onClick={() => handleDelete(image)}>
+            <Image width={300} height={300} src={image} alt={image} />
           </div>
-        );
-      }}
-    </CldUploadWidget>
+        ))}
+      </div>
+    </>
   );
 };
 
