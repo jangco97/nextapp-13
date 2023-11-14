15563d56d289051f707aba97f637c6aae6ce1112
jangco97 <qkrnslehd@naver.com>
2023-11-14 17:29:21 +0900

    카테고리 constants파일로 넘기기

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,244 @@
 export const PRODUCTS_PER_PAGE = 12;
 export const BOARDS_PER_PAGE = 10;
+
+export const mainCategories = [
+  {
+    label: "도서/문구",
+    categoryId: "1",
+    secondCategory: [
+      {
+        label: "학습/참고서",
+        categoryId: "101",
+      },
+      {
+        label: "수험서/자격증",
+        categoryId: "102",
+      },
+      {
+        label: "외국어",
+        categoryId: "103",
+      },
+      {
+        label: "컴퓨터/인터넷",
+        categoryId: "104",
+      },
+      {
+        label: "대학교재",
+        categoryId: "105",
+      },
+      {
+        label: "인터넷강의",
+        categoryId: "106",
+      },
+    ],
+  },
+  {
+    label: "리빙/생활",
+    categoryId: "2",
+    secondCategory: [
+      {
+        label: "주방용품",
+        categoryId: "107",
+      },
+      {
+        label: "식품",
+        categoryId: "108",
+      },
+      {
+        label: "욕실용품",
+        categoryId: "109",
+      },
+      {
+        label: "생활잡화",
+        categoryId: "110",
+      },
+    ],
+  },
+  {
+    label: "패션의류",
+    categoryId: "3",
+    secondCategory: [
+      {
+        label: "여성의류",
+        categoryId: "111",
+      },
+      {
+        label: "남성의류",
+        categoryId: "112",
+      },
+      {
+        label: "남녀공용",
+        categoryId: "113",
+      },
+    ],
+  },
+  {
+    label: "패션잡화",
+    categoryId: "4",
+    secondCategory: [
+      {
+        label: "여성신발",
+        categoryId: "114",
+      },
+      {
+        label: "남성신발",
+        categoryId: "115",
+      },
+      {
+        label: "여성시계",
+        categoryId: "116",
+      },
+      {
+        label: "남성시계",
+        categoryId: "117",
+      },
+    ],
+  },
+  {
+    label: "모바일/태블릿",
+    categoryId: "5",
+    secondCategory: [
+      {
+        label: "스마트폰",
+        categoryId: "118",
+      },
+      {
+        label: "태블릿",
+        categoryId: "119",
+      },
+      {
+        label: "액세서리",
+        categoryId: "120",
+      },
+    ],
+  },
+  {
+    label: "pc/노트북",
+    categoryId: "6",
+    secondCategory: [
+      {
+        label: "데스크탑",
+        categoryId: "121",
+      },
+      {
+        label: "노트북",
+        categoryId: "122",
+      },
+      {
+        label: "모니터",
+        categoryId: "123",
+      },
+      {
+        label: "주변기기",
+        categoryId: "124",
+      },
+    ],
+  },
+  {
+    label: "무료나눔",
+    categoryId: "7",
+  },
+  {
+    label: "기타",
+    categoryId: "8",
+  },
+];
+
+export const subCategories = [
+  {
+    label: "학습/참고서",
+    categoryId: "101",
+  },
+  {
+    label: "수험서/자격증",
+    categoryId: "102",
+  },
+  {
+    label: "외국어",
+    categoryId: "103",
+  },
+  {
+    label: "컴퓨터/인터넷",
+    categoryId: "104",
+  },
+  {
+    label: "대학교재",
+    categoryId: "105",
+  },
+  {
+    label: "인터넷강의",
+    categoryId: "106",
+  },
+  {
+    label: "주방용품",
+    categoryId: "107",
+  },
+  {
+    label: "식품",
+    categoryId: "108",
+  },
+  {
+    label: "욕실용품",
+    categoryId: "109",
+  },
+  {
+    label: "생활잡화",
+    categoryId: "110",
+  },
+  {
+    label: "여성의류",
+    categoryId: "111",
+  },
+  {
+    label: "남성의류",
+    categoryId: "112",
+  },
+  {
+    label: "남녀공용",
+    categoryId: "113",
+  },
+  {
+    label: "여성신발",
+    categoryId: "114",
+  },
+  {
+    label: "남성신발",
+    categoryId: "115",
+  },
+  {
+    label: "여성시계",
+    categoryId: "116",
+  },
+  {
+    label: "남성시계",
+    categoryId: "117",
+  },
+  {
+    label: "스마트폰",
+    categoryId: "118",
+  },
+  {
+    label: "태블릿",
+    categoryId: "119",
+  },
+  {
+    label: "액세서리",
+    categoryId: "120",
+  },
+  {
+    label: "데스크탑",
+    categoryId: "121",
+  },
+  {
+    label: "노트북",
+    categoryId: "122",
+  },
+  {
+    label: "모니터",
+    categoryId: "123",
+  },
+  {
+    label: "주변기기",
+    categoryId: "124",
+  },
+];
