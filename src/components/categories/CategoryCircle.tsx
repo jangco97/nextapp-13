87e7b9601f89da094a989ffde6e0747795c641ea
jangco97 <qkrnslehd@naver.com>
2023-11-20 17:37:34 +0900

    categorycircel 크기 변경 및 정렬

--- src/components/categories/CategoryCircle.tsx
+++ src/components/categories/CategoryCircle.tsx
@@ -5,39 +5,41 @@ import Link from "next/link";
 const CategoryCircle = ({ currentLabel }: { currentLabel: string | null }) => {
   return (
     <>
-      <div className="grid grid-cols-4 gap-5 mb-5">
+      <div className="grid grid-cols-4 gap-5 mb-5 place-items-center">
         {mainCategories.map((item) => (
           <>
             {item.label === currentLabel &&
               item?.secondCategory?.map((subitem) => (
-                <Link
+                <div
                   key={subitem.label}
-                  href={`/?category=${subitem.categoryId}&label=${item.label}&subLabel=${subitem.label}`}
+                  className="flex items-center justify-center  w-24 h-10  rounded-md bg-gradient-to-r from-blue-400  via-indigo-400 to-indigo-500 p-2 hover:bg-gray-500/50 cursor-pointer"
                 >
-                  <div className="flex items-center justify-center  w-24 h-10  rounded-md bg-gradient-to-r from-blue-400  via-indigo-400 to-indigo-500 p-2 hover:bg-gray-500/50 cursor-pointer">
+                  <Link
+                    href={`/?category=${subitem.categoryId}&label=${item.label}&subLabel=${subitem.label}`}
+                  >
                     <span className="block text-xs text-white px-4 py-2 font-semibold rounded-md ">
                       {subitem.label}
                     </span>
-                  </div>
-                </Link>
+                  </Link>
+                </div>
               ))}
           </>
         ))}
       </div>
-      <div className="grid grid-cols-4 gap-5 w-full">
+      <div className="grid grid-cols-4 gap-5 place-items-center">
         {mainCategories.map((item) => (
           <>
-            <Link
+            <div
               key={item.label}
-              href={`/?category=${item.categoryId}&label=${item.label}`}
+              className="flex flex-col items-center justify-center w-20 h-20 bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-500 p-2 border-2 rounded-full hover:bg-gray-500/50 cursor-pointer"
             >
-              <div className="flex flex-col items-center justify-center w-24 h-24 bg-gradient-to-r from-indigo-300 via-indigo-400 to-indigo-500 p-2 border-2 rounded-full hover:bg-gray-500/50 cursor-pointer">
-                <div className="flex flex-col items-center justify-center text-sm  text-white  font-semibold rounded-full">
+              <Link href={`/?category=${item.categoryId}&label=${item.label}`}>
+                <div className="flex flex-col items-center justify-center text-xs text-white  font-semibold rounded-full">
                   <CategoryIcon label={item.label} />
                   {item.label}
                 </div>
-              </div>
-            </Link>
+              </Link>
+            </div>
           </>
         ))}
       </div>
