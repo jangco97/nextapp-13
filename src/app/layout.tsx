5eb0d7a7a2c93e2bd3ba90195f3520e803835391
jangco97 <qkrnslehd@naver.com>
2024-02-01 15:56:01 +0900

    style: 로고 추가하기

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "줄건중고",
   description: "중고거래 웹 애플리케이션",
+  icons: {
+    icon: "/marketicon.png",
+  },
 };
 
 export default async function RootLayout({
