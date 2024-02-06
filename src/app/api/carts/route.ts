c435a8fa89a012448cbf9d2b8f41889f5d2f2281
jangco97 <qkrnslehd@naver.com>
2024-02-06 16:43:00 +0900

    fix: api/carts 수정

--- src/app/api/carts/route.ts
+++ src/app/api/carts/route.ts
@@ -1,11 +1,10 @@
-import { NextResponse, NextRequest } from "next/server";
-import prisma from "@/app/libs/prismadb";
-import getCurrentUser from "@/app/actions/getCurrentUser";
-export const dynamic = "force-dynamic";
+import { NextResponse, NextRequest } from 'next/server';
+import prisma from '@/app/libs/prismadb';
+import getCurrentUser from '@/app/actions/getCurrentUser';
 export async function GET(request: NextRequest) {
   const currentUser = await getCurrentUser();
   if (!currentUser) {
-    return NextResponse.json({ message: "User not found" }, { status: 404 });
+    return NextResponse.json({ message: 'User not found' }, { status: 404 });
   }
   const carts = await prisma.product.findMany({
     where: {
