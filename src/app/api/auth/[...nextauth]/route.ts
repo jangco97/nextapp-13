63511a81f298822cf430d39187d1a61b8e7e0dfb
jangco97 <qkrnslehd@naver.com>
2024-02-13 15:12:06 +0900

    fix: 로그인 버그 수정

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,63 +1,61 @@
-import NextAuth, { NextAuthOptions } from "next-auth";
-import CredentialsProvider from "next-auth/providers/credentials";
-import bcrypt from "bcryptjs";
-import prisma from "@/app/libs/prismadb";
-export const authOptions: NextAuthOptions = {
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        // Add logic here to look up the user from the credentials supplied
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Invalid Credentials");
-        }
-        const user = await prisma.user.findUnique({
-          where: {
-            email: credentials.email,
-          },
-        });
-        if (!user || !user.hashedPassword) {
-          throw new Error("Invalid Credentials");
-        }
-        if (!user.emailVerified) {
-          throw new Error("Please verify your email");
-        }
-        const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword);
-        if (!isCorrectPassword) {
-          throw new Error("Invalid credentials");
-        }
-        return user;
-      },
-    }),
-  ],
-  secret: process.env.NEXTAUTH_SECRET!,
-  pages: {
-    signIn: "/auth/login",
-  },
-  session: {
-    strategy: "jwt",
-  },
-  jwt: {
-    secret: process.env.JWT_SECRET!,
-    maxAge: 30 * 24 * 60 * 60, // 30 days
-  },
-  callbacks: {
-    async jwt({ token, user }) {
-      return { ...token, ...user };
-    },
-    async session({ session, token }) {
-      session.user = token;
-      return session;
-    },
-  },
-};
+import NextAuth from "next-auth";
+import { authOptions } from "@/lib/auth";
+// export const authOptions: NextAuthOptions = {
+//   providers: [
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         email: {
+//           label: "Email",
+//           type: "email",
+//         },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials) {
+//         // Add logic here to look up the user from the credentials supplied
+//         if (!credentials?.email || !credentials?.password) {
+//           throw new Error("Invalid Credentials");
+//         }
+//         const user = await prisma.user.findUnique({
+//           where: {
+//             email: credentials.email,
+//           },
+//         });
+//         if (!user || !user.hashedPassword) {
+//           throw new Error("Invalid Credentials");
+//         }
+//         if (!user.emailVerified) {
+//           throw new Error("Please verify your email");
+//         }
+//         const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword);
+//         if (!isCorrectPassword) {
+//           throw new Error("Invalid credentials");
+//         }
+//         return user;
+//       },
+//     }),
+//   ],
+//   secret: process.env.NEXTAUTH_SECRET!,
+//   pages: {
+//     signIn: "/auth/login",
+//   },
+//   session: {
+//     strategy: "jwt",
+//   },
+//   jwt: {
+//     secret: process.env.JWT_SECRET!,
+//     maxAge: 30 * 24 * 60 * 60, // 30 days
+//   },
+//   callbacks: {
+//     async jwt({ token, user }) {
+//       return { ...token, ...user };
+//     },
+//     async session({ session, token }) {
+//       session.user = token;
+//       return session;
+//     },
+//   },
+// };
 
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
---
+++ src/lib/auth.ts
@@ -0,0 +1,74 @@
+import prisma from "@/app/libs/prismadb";
+import bcrypt from "bcryptjs";
+import CredentialsProvider from "next-auth/providers/credentials";
+import type { NextAuthOptions } from "next-auth";
+
+export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "/auth/login",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  jwt: {
+    secret: process.env.JWT_SECRET!,
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  secret: process.env.NEXTAUTH_SECRET!,
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Invalid Credentials");
+        }
+        const user = await prisma.user.findUnique({
+          where: {
+            email: credentials.email,
+          },
+        });
+        if (!user || !user.hashedPassword) {
+          throw new Error("Invalid Credentials");
+        }
+        if (!user.emailVerified) {
+          throw new Error("Please verify your email");
+        }
+        const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword);
+        if (!isCorrectPassword) {
+          throw new Error("Invalid credentials");
+        }
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    session: ({ session, token }) => {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: token.id,
+          randomKey: token.randomKey,
+        },
+      };
+    },
+    jwt: ({ token, user }) => {
+      if (user) {
+        const u = user as unknown as any;
+        return {
+          ...token,
+          id: u.id,
+          randomKey: u.randomKey,
+        };
+      }
+      return token;
+    },
+  },
+};
